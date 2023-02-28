e598edd985fe7059f2a773ad7ba425df0d74cb50
Theresa <tmannschatz@gmail.com>
2023-02-28 21:12:04 +0100

    Test adding access token refresh

--- lib/active_storage/service/dropbox_service.rb
+++ lib/active_storage/service/dropbox_service.rb
@@ -93,9 +93,26 @@ module ActiveStorage
         end
       end
 
+      # TODO: need to store the access_token as long as it is valid
+      def access_token
+        url = 'https://api.dropbox.com/oauth2/token'
+
+        # authenticate
+        payload = {
+          grant_type: 'refresh_token',
+          refresh_token: Rails.application.credentials.dig(:dropbox, :refresh_token),
+          client_id: Rails.application.credentials.dig(:dropbox, :app_key),
+          client_secret: Rails.application.credentials.dig(:dropbox, :app_secret)
+        }
+
+        # get access_token
+        response = RestClient.post(url, payload)
+        JSON.parse(response.body)['access_token']
+      end
 
       def client
-        @client ||= DropboxApi::Client.new(access_token: OAuth2::AccessToken.new('client', config.fetch(:access_token)))
+        # @client ||= DropboxApi::Client.new(access_token: OAuth2::AccessToken.new('client', config.fetch(:access_token)))
+        @client ||= DropboxApi::Client.new(access_token: OAuth2::AccessToken.new('client', access_token))
       end
   end
 end
